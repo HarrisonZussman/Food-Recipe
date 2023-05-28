6ee29b534c1625b0e2ec1efd60a19d39943a54a7
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-05-28 19:10:23 -0400

    link to random

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,13 @@
 // rfce (tab)
+
 function Popular() {
+
+const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`)
+}
+
+
+
     return <div>Popular</div>
 }
 
