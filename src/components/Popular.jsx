de5af85f3ca5703e23e3524bbd98b9ad94b8a48f
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-05-28 19:24:45 -0400

    connect to random recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,20 @@
 // rfce (tab)
 
+import { useEffect, useState } from "react";
+
 function Popular() {
 
+    const [popular, setPopular] = useState([]);
+
+    useEffect(()  => {
+        getPopular();
+    },[]);
+
 const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`)
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+    const data = await api.json();
+    setPopular(data.recipes)
+
 }
 
 
