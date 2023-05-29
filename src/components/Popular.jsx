dc18af1db6dced7ed2ff9de70e9a56651363c61e
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-05-29 19:16:01 -0400

    localstorage in popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,30 +2,40 @@
 
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import {Splide, SplideSlide} from "@splidejs/react-splide";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
 
     const [popular, setPopular] = useState([]);
 
-    useEffect(()  => {
+    useEffect(() => {
         getPopular();
-    },[]);
+    }, []);
 
-const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
-    const data = await api.json();
-    setPopular(data.recipes)
+    const getPopular = async () => {
 
-}
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+            const data = await api.json();
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+            console.log(data.recipes);
+        }
+
+
+    }
 
 
 
     return <div>
-            <Wrapper>
-                <h3>Popular Picks</h3>
-                <Splide
+        <Wrapper>
+            <h3>Popular Picks</h3>
+            <Splide
                 options={{
                     perPage: 5,
                     arrows: false,
@@ -33,20 +43,20 @@ const getPopular = async () => {
                     drag: "free",
                     gap: "5rem",
                 }}
-                >
+            >
                 {popular.map((recipe) => {
-                    return(
-                        <SplideSlide>
-                        <Card>
-                            <p>{recipe.title}</p>
-                            <img src={recipe.image} alt={recipe.title} />
-                            <Gradient />
-                        </Card>
+                    return (
+                        <SplideSlide key={recipe.id}>
+                            <Card>
+                                <p>{recipe.title}</p>
+                                <img src={recipe.image} alt={recipe.title} />
+                                <Gradient />
+                            </Card>
                         </SplideSlide>
                     );
                 })}
-                </Splide>
-            </Wrapper>
+            </Splide>
+        </Wrapper>
     </div>
 }
 
@@ -86,12 +96,12 @@ p{
 }
 `;
 
-const Gradient = styled.div
+const Gradient = styled.div`
 z-index: 3;
 position: absolute;
 width: 100%;
 height: 100%;
 background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
-
+`;
 
 export default Popular
