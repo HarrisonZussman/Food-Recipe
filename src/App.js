08aadb345a81ba404fefc6ae64bc6da710450467
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-05-30 15:44:48 -0400

    added category

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import pages from "./pages/pages";
+import Pages from "./pages/pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
       <Pages />
     </div>
   );
